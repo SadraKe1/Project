3d66ac3c4760a41896564f1b053c9a32e62616f6
JustHeroic <113134339+JustHeroic@users.noreply.github.com>
2022-11-29 21:20:36 -0500

    Haroon's commit

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
+//requests home page and displays it
 module.exports.displayHomepage = function(req, res, next) {
     res.render('index', {   title: "Home"  });
   }
 
-
+//requests about page and displays it
   module.exports.displayAboutPage = (req,res,next)=>{
     res.render('index', {title:'About'});
   }
-
+//requests product page and displays it
   module.exports.displayProductPage = (req,res,next)=>{
     res.render('index', {title:'Products'});
   }
-
+//requests service page and displays it
   module.exports.displayServicePage = (req,res,next)=>{
     res.render('index', {title:'Services'});
   }
-
+//displays comments page//
   module.exports.displayContactPage = (req,res,next)=>{
     res.render('index', {title:'Contact'});
   }
--- server/models/JobSearch.js
+++ server/models/JobSearch.js
@@ -4,13 +4,13 @@ let mongoose= require('mongoose');
 
 let JobSearchModel=mongoose.Schema({
 
-    jobtitle:String, 
-    requirements:String,
-    hourlysalary:String,
-    availability: String,
+    jobtitle:String, // holds a string value for job title for the user to enter
+    requirements:String, // holds a string value for requirement for the user to enter
+    hourlysalary:String, // holds a string value for houlry salary for the user to enter
+    availability: String, // holds a string value for availablility for the user to enter
 },
 {
-collection: "JobSearchs"
+collection: "JobSearchs" //creates the collection job searches
 }
 );
 module.exports = mongoose.model('JobSearch',JobSearchModel);
